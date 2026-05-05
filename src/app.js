4c5a4fb5a13aaae3732f5514a40a38db403ac4ce
ZiramBayam <ramzialfito32@gmail.com>
2026-05-05 19:03:39 +0700

    chore: configure express app startup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express()
 
 const projectsRoute = require('./features/projects/routes/project.routes')
 const biddingRoute = require('./features/bidding/routes/bidding.routes')
+
+app.use(express.json())
+
 app.use('/api/projects', projectsRoute)
 app.use('/api/bidding', biddingRoute)
 
